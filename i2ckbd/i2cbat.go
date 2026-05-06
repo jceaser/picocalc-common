f084c2574cf08191c8208acaebbcf5401e034dc9
Thomas Cherry <Thomas.Cherry@gmail.com>
2026-05-06 12:28:10 -0400

    Removed unused libs

--- i2ckbd/i2cbat.go
+++ i2ckbd/i2cbat.go
@@ -5,18 +5,11 @@
 
 package i2ckbd
 
-import (
-	"fmt"
-	"machine"
-)
-
-
 // ***************************************************************************80
 // Battery
 
 func (kbd *I2CKbd) ReadBattery() int16 {
     msg := []byte{0x0B} // Command to send
-	//kbd.write[0] = 0x0B
 	
     err := kbd.i2c.Tx(i2cKbdAddr, msg, kbd.read)
 	if err != nil {
